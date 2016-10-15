408d4c41b66368f31697586671fe319f9a0a6fef
RobbieGM <rgm020103@icloud.com>
2016-10-15 13:52:06 -0400

    Added files

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RGBSensorTest.java
@@ -0,0 +1,29 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+@Autonomous(name="Pushbot: RGBSensorTest", group="Pushbot")
+public class RGBSensorTest extends LinearOpMode {
+    //RMHardwarePushbot robot = new RMHardwarePushbot();
+    ColorSensor rgbs;
+    //DcMotor leftMotor = null;
+    //DcMotor rightMotor = null;
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //robot.init(hardwareMap);
+        waitForStart();
+        rgbs = hardwareMap.colorSensor.get("colorsensor");
+        rgbs.enableLed(false);
+        while (opModeIsActive()) {
+            telemetry.addData("Red: ", rgbs.red());
+            telemetry.addData("Green: ", rgbs.green());
+            telemetry.addData("Blue: ", rgbs.blue());
+            telemetry.update();
+            idle();
+        }
+        rgbs.enableLed(false);
+        stop();
+    }
+}
