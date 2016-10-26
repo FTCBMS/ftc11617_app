79472521795da979a20dc1df9614f420b32df9db
Robbie Moore <null>
2016-10-26 18:22:21 -0400

    Added robot hardware object

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
@@ -18,9 +18,10 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
  */
 @Autonomous(name="VO", group="Vuforia")
 public class RMVO extends LinearOpMode {
-    Push robot = new Push();
+    RMHardwarePushbot robot = new RMHardwarePushbot();
     @Override
     public void runOpMode() throws InterruptedException {
+        robot.init(hardwareMap);
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         params.vuforiaLicenseKey = "AfQTqo//////AAAAGavZWqzkWk2doCWRXf8Y5Vc3FRE8eVl+2UyeBBNtFWIOdD1y0yVVXsz9vSOQKnpzt/QTHaHe+wQ/ulCYHGMxLWC7rtTBI7+bmWCTlOm8Sz9iZLiQAZZxedoDVzoPjTepbhHJBipbxmUrqPhpp/cyIAqqP0w9pfGzX+0r7aJP8RU2Fvayqe5pr6B3WK91sHOkuhL0SV6bQGqjcnetWvgBs+pDJm/PQon8QKQXw3w0cbJhyd+P2w1Gr92w+ZX6ctJh0AkCKz4KIvkh6fd1ND/qNGmp0mDHwOhIwuZIIeNjmBDYYIOfWH3l4F4HQHWmVGhIwV19woHA0PyCWJODyXCdSt3olvtBSz3cJj42AFIRXITI";
