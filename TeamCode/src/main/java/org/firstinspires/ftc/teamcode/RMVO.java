cb7343c4af72d68e47338559a8d5c041c6b41c42
Robbie Moore <null>
2016-10-29 16:01:43 -0400

    Track pictures and turn with extrene precision

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
@@ -2,6 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.vuforia.HINT;
 import com.vuforia.Vuforia;
 
@@ -19,9 +21,19 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 @Autonomous(name="RMVO", group="Vuforia")
 public class RMVO extends LinearOpMode {
     RMHardwarePushbot robot = new RMHardwarePushbot();
+    private ElapsedTime runtime = new ElapsedTime();
+
+    static final double COUNTS_PER_MOTOR_REV = 1440;    // eg: TETRIX Motor Encoder
+    static final double DRIVE_GEAR_REDUCTION = 2.0;     // This is < 1.0 if geared UP
+    static final double WHEEL_DIAMETER_INCHES = 4.0;     // For figuring circumference
+    static final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
+            (WHEEL_DIAMETER_INCHES * 3.1415);
+    static final double DRIVE_SPEED = 0.6;
+    static final double TURN_SPEED = 0.5;
+
     @Override
     public void runOpMode() throws InterruptedException {
-        //robot.init(hardwareMap);
+        robot.init(hardwareMap);
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         params.vuforiaLicenseKey = "AfQTqo//////AAAAGavZWqzkWk2doCWRXf8Y5Vc3FRE8eVl+2UyeBBNtFWIOdD1y0yVVXsz9vSOQKnpzt/QTHaHe+wQ/ulCYHGMxLWC7rtTBI7+bmWCTlOm8Sz9iZLiQAZZxedoDVzoPjTepbhHJBipbxmUrqPhpp/cyIAqqP0w9pfGzX+0r7aJP8RU2Fvayqe5pr6B3WK91sHOkuhL0SV6bQGqjcnetWvgBs+pDJm/PQon8QKQXw3w0cbJhyd+P2w1Gr92w+ZX6ctJh0AkCKz4KIvkh6fd1ND/qNGmp0mDHwOhIwuZIIeNjmBDYYIOfWH3l4F4HQHWmVGhIwV19woHA0PyCWJODyXCdSt3olvtBSz3cJj42AFIRXITI";
@@ -37,14 +49,19 @@ public class RMVO extends LinearOpMode {
         beacons.get(2).setName("Lego");
         beacons.get(3).setName("Gears");
 
+        robot.leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         waitForStart();
         // The following awesome code is created by Robbie Moore.
         // Copyright (c) 2016 Robbie Moore.
         beacons.activate();
-        while (opModeIsActive()) {
-            //robot.rightMotor.setPower(0);
-            //robot.leftMotor.setPower(0);
+        encoderDrive(TURN_SPEED,   5, -5, 4.0);
+        robot.leftMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        robot.rightMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        whole_thing:while (opModeIsActive()) {
+            robot.rightMotor.setPower(0.2);
+            robot.leftMotor.setPower(0.2);
             int i = 0;
             for (VuforiaTrackable beac : beacons) {
                 if (i == 0) {
@@ -59,11 +76,15 @@ public class RMVO extends LinearOpMode {
                         telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
                         double positionOnScreen = translation.get(1); // = translation.get(0) for upright phones
                         // ^^ IMPORTANT ^^: phone must be right-side-down or it will move away from the picture!
-                        double adjust = positionOnScreen / 250;
-                        adjust = clamp(adjust, 0, 0.3);
-                        if (translation.get(2) > -450) { // If z axis (distance) > ~8in (approx.)
-                            robot.rightMotor.setPower(0.4-adjust);
-                            robot.leftMotor.setPower(0.4+adjust);
+                        double adjust = positionOnScreen / 70;
+                        adjust = clamp(adjust, -0.2, 0.3);
+                        if (translation.get(2) < -300) { // If z axis (distance) > ~8in (approx.)
+                            robot.rightMotor.setPower(0.4 - adjust);
+                            robot.leftMotor.setPower(0.4 + adjust);
+                        } else {
+                            robot.rightMotor.setPower(0);
+                            robot.leftMotor.setPower(0);
+                            break whole_thing;
                         }
                         /* (positionOnScreen > 10) { // Right side of screen
                             robot.rightMotor.setPower(0.3);
@@ -75,12 +96,15 @@ public class RMVO extends LinearOpMode {
                             robot.leftMotor.setPower(0.5);
                             robot.rightMotor.setPower(0.5);
                         }*/
-                        int idles = 0;
+                        /*int idles = 0;
                         for (int j=0;j<idles;j++) {
                             idle();
-                        }
-                    }else{
+                        }*/
+                        sleep(200);
+                    } else {
                         telemetry.addData("No image found", "");
+                        robot.leftMotor.setPower(0.2);
+                        robot.rightMotor.setPower(0.2);
                     }
                 }
                 i++;
@@ -90,7 +114,51 @@ public class RMVO extends LinearOpMode {
         }
         stop();
     }
+
     public double clamp(double x, double min, double max) {
         return Math.min(max, Math.max(min, x));
+    }
+
+    public void encoderDrive(double speed,
+                             double leftInches, double rightInches,
+                             double timeoutS) throws InterruptedException {
+        int newLeftTarget;
+        int newRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive()) {
+
+            // Determine new target position, and pass to motor controller
+            newLeftTarget = robot.leftMotor.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRightTarget = robot.rightMotor.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            robot.leftMotor.setTargetPosition(newLeftTarget);
+            robot.rightMotor.setTargetPosition(newRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.leftMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rightMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.leftMotor.setPower(Math.abs(speed));
+            robot.rightMotor.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while (opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.leftMotor.isBusy() && robot.rightMotor.isBusy())) {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newLeftTarget, newRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.leftMotor.getCurrentPosition(),
+                        robot.rightMotor.getCurrentPosition());
+                telemetry.update();
+
+                // Allow time for other processes to run.
+                idle();
+            }
+
+        }
     }
 }
