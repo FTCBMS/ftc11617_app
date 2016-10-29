22dc49cc6a4e1b5b5d095a2f46bf504e80622d43
Robbie Moore <null>
2016-10-29 13:49:30 -0400

    Picture tracking program is PID-based (without the I and D) to move more smoothly.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RMVO.java
@@ -21,11 +21,11 @@ public class RMVO extends LinearOpMode {
     RMHardwarePushbot robot = new RMHardwarePushbot();
     @Override
     public void runOpMode() throws InterruptedException {
-        robot.init(hardwareMap);
+        //robot.init(hardwareMap);
         VuforiaLocalizer.Parameters params = new VuforiaLocalizer.Parameters(R.id.cameraMonitorViewId);
         params.cameraDirection = VuforiaLocalizer.CameraDirection.BACK;
         params.vuforiaLicenseKey = "AfQTqo//////AAAAGavZWqzkWk2doCWRXf8Y5Vc3FRE8eVl+2UyeBBNtFWIOdD1y0yVVXsz9vSOQKnpzt/QTHaHe+wQ/ulCYHGMxLWC7rtTBI7+bmWCTlOm8Sz9iZLiQAZZxedoDVzoPjTepbhHJBipbxmUrqPhpp/cyIAqqP0w9pfGzX+0r7aJP8RU2Fvayqe5pr6B3WK91sHOkuhL0SV6bQGqjcnetWvgBs+pDJm/PQon8QKQXw3w0cbJhyd+P2w1Gr92w+ZX6ctJh0AkCKz4KIvkh6fd1ND/qNGmp0mDHwOhIwuZIIeNjmBDYYIOfWH3l4F4HQHWmVGhIwV19woHA0PyCWJODyXCdSt3olvtBSz3cJj42AFIRXITI";
-        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.TEAPOT;
+        params.cameraMonitorFeedback = VuforiaLocalizer.Parameters.CameraMonitorFeedback.AXES;
 
         VuforiaLocalizer vuforia = ClassFactory.createVuforiaLocalizer(params);
         Vuforia.setHint(HINT.HINT_MAX_SIMULTANEOUS_IMAGE_TARGETS, 4);
@@ -43,8 +43,8 @@ public class RMVO extends LinearOpMode {
         // Copyright (c) 2016 Robbie Moore.
         beacons.activate();
         while (opModeIsActive()) {
-            robot.rightMotor.setPower(0);
-            robot.leftMotor.setPower(0);
+            //robot.rightMotor.setPower(0);
+            //robot.leftMotor.setPower(0);
             int i = 0;
             for (VuforiaTrackable beac : beacons) {
                 if (i == 0) {
@@ -52,19 +52,32 @@ public class RMVO extends LinearOpMode {
                     if (pose != null) {
                         VectorF translation = pose.getTranslation();
                         telemetry.addData(beac.getName() + "-Translation", translation);
-                        telemetry.addData("X offset (?)", translation.get(0));
+                        telemetry.addData("X offset", translation.get(0));
+                        telemetry.addData("Y offset", translation.get(1));
+                        telemetry.addData("Z offset (?)", translation.get(2));
                         double degreesToTurn = Math.toDegrees(Math.atan2(translation.get(1), translation.get(2)));
                         telemetry.addData(beac.getName() + "-Degrees", degreesToTurn);
-                        double positionOnScreen = translation.get(0);
-                        if (positionOnScreen > 10) { // Right side of screen
-                            robot.rightMotor.setPower(0.2);
-                            robot.leftMotor.setPower(0.6);
-                        }else if (positionOnScreen < -10 ){ // Left side of screen
-                            robot.rightMotor.setPower(0.6);
-                            robot.leftMotor.setPower(0.2);
-                        }else{ // Near the middle
-                            robot.leftMotor.setPower(0.3);
+                        double positionOnScreen = translation.get(1); // = translation.get(0) for upright phones
+                        // ^^ IMPORTANT ^^: phone must be right-side-down or it will move away from the picture!
+                        double adjust = positionOnScreen / 250;
+                        adjust = clamp(adjust, 0, 0.3);
+                        if (translation.get(2) > -450) { // If z axis (distance) > ~8in (approx.)
+                            robot.rightMotor.setPower(0.4-adjust);
+                            robot.leftMotor.setPower(0.4+adjust);
+                        }
+                        /* (positionOnScreen > 10) { // Right side of screen
                             robot.rightMotor.setPower(0.3);
+                            robot.leftMotor.setPower(0.9);
+                        }else if (positionOnScreen < -10 ){ // Left side of screen
+                            robot.rightMotor.setPower(0.9);
+                            robot.leftMotor.setPower(0.3);
+                        }else{ // Near the middle
+                            robot.leftMotor.setPower(0.5);
+                            robot.rightMotor.setPower(0.5);
+                        }*/
+                        int idles = 0;
+                        for (int j=0;j<idles;j++) {
+                            idle();
                         }
                     }else{
                         telemetry.addData("No image found", "");
@@ -76,5 +89,8 @@ public class RMVO extends LinearOpMode {
             idle();
         }
         stop();
+    }
+    public double clamp(double x, double min, double max) {
+        return Math.min(max, Math.max(min, x));
     }
 }
